7d5925d35a8481bbb4148b82615a74b6652f3785
Shreya Majhi <shreyamajhi88@gmail.com>
2025-12-03 15:13:35 +0530

    First Commit

---
+++ src/test/java/github/GithubPracticeTest.java
@@ -0,0 +1,16 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GithubPracticeTest {
+	@Test
+	public void githubPracticeTest() {
+		Reporter.log("Hi Github", true);
+		//getting parameter from system
+		//String BROWSER = System.getProperty("browser");
+		//System.out.println(BROWSER);
+	}
+
+}
+
